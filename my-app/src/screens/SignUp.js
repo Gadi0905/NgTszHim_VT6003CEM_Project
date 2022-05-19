d12e8703e512f1f30736a848099ba9c37454fe55
Gadi <gadi0905@gmail.com>
2022-05-19 23:22:42 +0800

    register function done

--- my-app/src/screens/SignUp.js
+++ my-app/src/screens/SignUp.js
@@ -1,33 +1,54 @@
-import React from 'react'
+import Axios from 'axios'
+import React, { useState } from 'react'
 // bootstrap components
-import Card from 'react-bootstrap/Card';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
+// import Form from 'react-bootstrap/Form';
+// import Button from 'react-bootstrap/Button';
 
 export default function SignUp() {
+    const url = "http://localhost:5000/employee";
+    const [data, setData] = useState({
+        name: "",
+        age: "",
+        sex: "",
+        email: "",
+        password: "",
+        sign_up_code: "",
+    })
+
+    function submit(e){
+        e.preventDefault();
+        Axios.post(url,{
+            name: data.name,
+            age: data.age,
+            sex: data.sex,
+            email: data.email,
+            password: data.password,
+            sign_up_code: data.sign_up_code
+        }).then(res => {
+            console.log(res.data)
+        })
+    }
+
+    function handle(e){
+        const newdata={...data}
+        newdata[e.target.id] = e.target.value
+        setData(newdata)
+        console.log(newdata)
+    }
+
     return (
         <div>
-            <Card>
-                <Card.Body>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                            <Form.Text className="text-muted">
-                                We'll never share your email with anyone else.
-                            </Form.Text>
-                        </Form.Group>
-
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
-                        </Form.Group>
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </Form>
-                </Card.Body>
-            </Card>
+            <br />
+            <form onSubmit={(e)=>submit(e)}>
+                <input onChange={(e)=>handle(e)} id="name" value={data.name} placeholder="name" type="text"></input><br /><br />
+                <input onChange={(e)=>handle(e)} id="age" value={data.age} placeholder="age" type="text"></input><br /><br />
+                <input onChange={(e)=>handle(e)} id="sex" value={data.sex} placeholder="sex" type="text"></input><br /><br />
+                <input onChange={(e)=>handle(e)} id="email" value={data.email} placeholder="email" type="email"></input><br /><br />
+                <input onChange={(e)=>handle(e)} id="password" value={data.password} placeholder="password" type="password"></input><br /><br />
+                <input onChange={(e)=>handle(e)} id="sign_up_code" value={data.sign_up_code} placeholder="sign_up_code" type="text"></input><br /><br />
+                <button>Submit</button>
+            </form>
         </div>
     )
 }
